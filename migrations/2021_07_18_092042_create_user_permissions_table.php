f2ef6daed118c744222a60a7e1cdd4ff8b7340df
parsa shahmaleki <parsampsh@gmail.com>
2021-07-21 15:20:51 +0430

    add migrations

---
+++ migrations/2021_07_18_092042_create_user_permissions_table.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of Simpermission.
+ *
+ * Copyright 2021 parsa shahmaleki <parsampsh@gmail.com>
+ *
+ * Simpermission project is Licensed Under MIT.
+ * For more information, please see the LICENSE file.
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserPermissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_permissions', function (Blueprint $table) {
+            $table->id();
+            $table->string('permission', 255);
+            $table->foreignId('user_id')
+                ->constrained()
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_permissions');
+    }
+}
